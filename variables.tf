f8d5902c24be691d4f83ca4f1baf2876bb4b9b87
Ubuntu <ubuntu@ip-172-31-68-27.ec2.internal>
2022-03-11 17:20:40 +0000

    more updates

--- variables.tf
+++ variables.tf
@@ -8,12 +8,30 @@ variable "access_ip" {
   default = "72.218.45.221/32"
 }
 
+variable "cloud9_ip" {
+  type    = string
+  default = "34.198.73.60/32"
+}
+
 variable "main_instance_type" {
-  type = string
+  type    = string
   default = "t2.micro"
 }
 
 variable "main_vol_size" {
-  type = number
+  type    = number
   default = 8
+}
+
+variable "main_instance_count" {
+  type    = number
+  default = 1
+}
+
+variable "key_name" {
+  type = string
+}
+
+variable "public_key_path" {
+  type = string
 }
