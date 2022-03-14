afe98573d29c7454ecfb43e01ae01e668b738a49
Ubuntu <ubuntu@ip-172-31-68-27.ec2.internal>
2022-03-14 10:11:32 +0000

    remove null resource

--- compute.tf
+++ compute.tf
@@ -60,12 +60,12 @@ resource "aws_instance" "mtc_main" {
 #   }
 # }
 
-resource "null_resource" "grafana_install" {
-  depends_on = [aws_instance.mtc_main]
-  provisioner "local-exec" {
-    command = "ansible-playbook -i aws_hosts --key-file /home/ubuntu/.ssh/mtckey playbooks/grafana.yml"
-  }
-}
+# resource "null_resource" "grafana_install" {
+#   depends_on = [aws_instance.mtc_main]
+#   provisioner "local-exec" {
+#     command = "ansible-playbook -i aws_hosts --key-file /home/ubuntu/.ssh/mtckey playbooks/grafana.yml"
+#   }
+# }
 
 output "grafana_access" {
   value = { for i in aws_instance.mtc_main[*] : i.tags.Name => "${i.public_ip}:3000" }
